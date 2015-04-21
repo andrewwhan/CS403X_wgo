d536489dfe732b3cafbe8c300bcf44885ebc2ab7
Andrew Han <awhan@wpi.edu>
2015-04-21 13:07:19 -0400

    Time change buttons now pull up timepickerdialogs

--- app/src/main/java/edu/wpi/wgo/CreateEvent.java
+++ app/src/main/java/edu/wpi/wgo/CreateEvent.java
@@ -1,25 +1,45 @@
 package edu.wpi.wgo;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 
 public class CreateEvent extends ActionBarActivity {
 
-    EditText startTime;
-    EditText endTime;
+    EditText startTimeField;
+    EditText endTimeField;
+    Calendar startTime;
+    Calendar endTime;
+
+    SimpleDateFormat date_format = new SimpleDateFormat("hh:mm a", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
-        startTime = (EditText)this.findViewById(R.id.editText3);
-        endTime = (EditText)this.findViewById(R.id.editText4);
-        startTime.setKeyListener(null);
-        endTime.setKeyListener(null);
+
+        startTime = new GregorianCalendar();
+        endTime = new GregorianCalendar();
+        endTime.set(Calendar.HOUR, endTime.get(Calendar.HOUR) + 1);
+
+        startTimeField = (EditText)this.findViewById(R.id.editText3);
+        endTimeField = (EditText)this.findViewById(R.id.editText4);
+        startTimeField.setKeyListener(null);
+        endTimeField.setKeyListener(null);
+        startTimeField.setText(date_format.format(startTime.getTime()));
+        endTimeField.setText(date_format.format(endTime.getTime()));
     }
 
 
@@ -43,5 +63,29 @@ public class CreateEvent extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeStartTime(View view){
+        TimePickerDialog tp = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                startTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                startTime.set(Calendar.MINUTE, minute);
+                startTimeField.setText(date_format.format(startTime.getTime()));
+            }
+        }, startTime.get(Calendar.HOUR_OF_DAY), startTime.get(Calendar.MINUTE), false);
+        tp.show();
+    }
+
+    public void changeEndTime(View view){
+        TimePickerDialog tp = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                endTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                endTime.set(Calendar.MINUTE, minute);
+                endTimeField.setText(date_format.format(endTime.getTime()));
+            }
+        }, endTime.get(Calendar.HOUR_OF_DAY), endTime.get(Calendar.MINUTE), false);
+        tp.show();
     }
 }
