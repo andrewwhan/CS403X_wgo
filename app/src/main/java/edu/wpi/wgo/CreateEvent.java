e81c71767970b6f7cfe9efce31c7461a25d2547f
Andrew Han <awhan@wpi.edu>
2015-04-21 21:07:14 -0400

    Merge photo stuff in and implement datepicker. Also gave meaningful ids to all the widgets.

--- app/src/main/java/edu/wpi/wgo/CreateEvent.java
+++ app/src/main/java/edu/wpi/wgo/CreateEvent.java
@@ -1,5 +1,6 @@
 package edu.wpi.wgo;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -10,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TimePicker;
@@ -24,13 +26,16 @@ public class CreateEvent extends ActionBarActivity {
 
     EditText startTimeField;
     EditText endTimeField;
+    EditText startDateField;
+    EditText endDateField;
     Calendar startTime;
     Calendar endTime;
     ImageView mImageView;
     Button capture;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    SimpleDateFormat date_format = new SimpleDateFormat("hh:mm a", Locale.US);
+    SimpleDateFormat time_format = new SimpleDateFormat("hh:mm a", Locale.US);
+    SimpleDateFormat date_format = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +46,22 @@ public class CreateEvent extends ActionBarActivity {
         endTime = new GregorianCalendar();
         endTime.set(Calendar.HOUR, endTime.get(Calendar.HOUR) + 1);
 
-        startTimeField = (EditText)this.findViewById(R.id.editText3);
-        endTimeField = (EditText)this.findViewById(R.id.editText4);
-
+        startTimeField = (EditText)this.findViewById(R.id.startTimeField);
+        endTimeField = (EditText)this.findViewById(R.id.endTimeField);
+        startDateField = (EditText)this.findViewById(R.id.startDateField);
+        endDateField = (EditText)this.findViewById(R.id.endDateField);
         startTimeField.setKeyListener(null);
         endTimeField.setKeyListener(null);
+        startDateField.setKeyListener(null);
+        endDateField.setKeyListener(null);
+        startTimeField.setText(time_format.format(startTime.getTime()));
+        endTimeField.setText(time_format.format(endTime.getTime()));
+        startDateField.setText(date_format.format(startTime.getTime()));
+        endDateField.setText(date_format.format(endTime.getTime()));
 
-        startTimeField.setText(date_format.format(startTime.getTime()));
-        endTimeField.setText(date_format.format(endTime.getTime()));
+        mImageView = (ImageView) this.findViewById(R.id.eventPhoto);
 
-        mImageView = (ImageView) this.findViewById(R.id.imageView);
-
-        capture = (Button) findViewById(R.id.button);
+        capture = (Button) findViewById(R.id.photoButton);
         capture.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -78,6 +87,8 @@ public class CreateEvent extends ActionBarActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageView.setImageBitmap(imageBitmap);
         }
+
+
     }
 
 
@@ -109,7 +120,7 @@ public class CreateEvent extends ActionBarActivity {
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 startTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
                 startTime.set(Calendar.MINUTE, minute);
-                startTimeField.setText(date_format.format(startTime.getTime()));
+                startTimeField.setText(time_format.format(startTime.getTime()));
             }
         }, startTime.get(Calendar.HOUR_OF_DAY), startTime.get(Calendar.MINUTE), false);
         tp.show();
@@ -121,9 +132,35 @@ public class CreateEvent extends ActionBarActivity {
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 endTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
                 endTime.set(Calendar.MINUTE, minute);
-                endTimeField.setText(date_format.format(endTime.getTime()));
+                endTimeField.setText(time_format.format(endTime.getTime()));
             }
         }, endTime.get(Calendar.HOUR_OF_DAY), endTime.get(Calendar.MINUTE), false);
         tp.show();
+    }
+
+    public void changeStartDate(View view){
+        DatePickerDialog dp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                startTime.set(Calendar.YEAR, year);
+                startTime.set(Calendar.MONTH, monthOfYear);
+                startTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                startDateField.setText(date_format.format(startTime.getTime()));
+            }
+        }, startTime.get(Calendar.YEAR), startTime.get(Calendar.MONTH), startTime.get(Calendar.DAY_OF_MONTH));
+        dp.show();
+    }
+
+    public void changeEndDate(View view){
+        DatePickerDialog dp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                endTime.set(Calendar.YEAR, year);
+                endTime.set(Calendar.MONTH, monthOfYear);
+                endTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                endDateField.setText(date_format.format(endTime.getTime()));
+            }
+        }, endTime.get(Calendar.YEAR), endTime.get(Calendar.MONTH), endTime.get(Calendar.DAY_OF_MONTH));
+        dp.show();
     }
 }
