107ea6988e01ba70bdaf0e5f2ec5be9803f4d83f
Andrew Han <awhan@wpi.edu>
2015-04-27 18:44:25 -0400

    Not done yet

--- app/src/main/java/edu/wpi/wgo/MainActivity.java
+++ app/src/main/java/edu/wpi/wgo/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.wpi.wgo;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -89,9 +93,19 @@ public class MainActivity extends ActionBarActivity {
     private AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView parent, View v, int position, long id) {
             // Do something in response to the click
-            Intent intent = new Intent(MainActivity.this, EventDetail.class);
-            intent.putExtra("Event", events.get(position));
-            startActivity(intent);
+//            Intent intent = new Intent(MainActivity.this, EventDetail.class);
+//            intent.putExtra("Event", events.get(position));
+//            startActivity(intent);
+            System.out.println("Hi");
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+//            builder.setSmallIcon(getResources().getIdentifier("goat.png", "drawable", getPackageName()));
+            builder.setSmallIcon(R.drawable.goat);
+            builder.setContentTitle("Event Goating On!");
+            builder.setContentText("People are coding in the zoo lab");
+            Notification not = builder.build();
+            System.out.println(not);
+            NotificationManager notServ = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            notServ.notify(257, not);
         }
     };
 
